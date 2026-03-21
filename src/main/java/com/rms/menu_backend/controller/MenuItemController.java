514f0aa89d6741acbdc3c4fa6aaa5e3a061a127c
MUTHUKRISHNAN N <23suca11@tcarts.in>
2026-03-21 18:45:11 +0530

    Update CORS origin to Azure static app URL

--- src/main/java/com/rms/menu_backend/controller/MenuItemController.java
+++ src/main/java/com/rms/menu_backend/controller/MenuItemController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/menu")
-@CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
+@CrossOrigin(origins = {"https://wonderful-sky-089039000.2.azurestaticapps.net/api/menu"}, allowCredentials = "true")
 public class MenuItemController {
 
     private final MenuItemRepository repo;
